7c5ecbab8edc763ddc3f41365ad3e29ee043bb77
Yellow Club 5 <hello@yellowclub.by>
2024-05-17 18:54:34 +0300

    lesson 3 - магические фильтры

--- handlers/user_privat.py
+++ handlers/user_privat.py
@@ -1,43 +1,48 @@
-from aiogram import types, Router
+from aiogram import types, Router, F
 from aiogram.filters import CommandStart, Command
-
 
 user_router = Router()
 
 
 @user_router.message(CommandStart())
 async def start_message(message: types.message):
-    await message.answer('Привет, это бот по продаже айфонов, какую модель вы хотели бы купить?')
+    await message.answer('Привет, это ботайфонов, по продаже  какую модель вы хотели бы купить?')
 
 
 @user_router.message(Command('catalog'))
+@user_router.message(F.text.lower() == 'меню')
 async def catalog(message: types.message):
     await message.answer('Каталог скоро будет добавлен')
 
 
 @user_router.message(Command('about'))
+@user_router.message(F.text.lower() == 'о нас')
 async def about(message: types.message):
-    await message.answer('Мы подберем для вас айфон по выгодной цене')
+    await message.answer('Мы подберем нужный вам айфон по выгодной цене')
 
 
 @user_router.message(Command('contacts'))
+@user_router.message(F.text.lower() == 'контакты')
 async def contacts(message: types.message):
     await message.answer('Позже здесь будут номера спонсоров')
 
 
 @user_router.message(Command('addresses'))
+@user_router.message(F.text.lower() == 'пункты выдачи')
 async def addresses(message: types.message):
-    await message.answer('Адрес')
+    await message.answer('Пункты выдачи')
 
 
+# @user_router.message(F.text)
+# @user_router.message(F.photo)
+# @user_router.message(F.text.lower() == 'доставка')
+# @user_router.message(F.text.lower().contains('доставк'))
+# @user_router.message(F.text.lower().startswith('как'))
+# @user_router.message(F.text.lower().endswith('?'))
+# @user_router.message(F.text.lower().startswith('как'), F.text.lower().endswith('?'))
+# @user_router.message( (F.text.lower().contains('цен')) | (F.text.lower().contains('стоимост')) )
 @user_router.message()
 async def echo(message: types.message):
-    await message.answer('бот пока что  находится в разработке')
-    user_text = message.text
-    await message.answer(user_text)
-
-
-
-
-
-
+    await message.answer('сработал магический фильтр')
+    # user_text = message.text
+    # await message.answer(user_text)
